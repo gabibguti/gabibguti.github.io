25bd6c9dc4f92004ce56ea9182f6148636eda23d
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-02-20 14:09:13 -0300

    feat: Add a main description of what i do

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import React, { ReactElement } from 'react'
 export function App(): ReactElement {
   return (
     <div className="bg-gradient-to-r from-light-blue via-light-blue to-white flex items-center px-24 w-screen h-screen">
-      <span className="text-5xl font-bold text-xiketic">
-        Gabriela Gutierrez
-      </span>
+      <div className="flex flex-col">
+        <span className="text-5xl font-bold text-xiketic">
+          Gabriela Gutierrez
+        </span>
+        <span className="text-base font-light text-xiketic pt-4">
+          Front-end developer
+        </span>
+      </div>
     </div>
   )
 }
