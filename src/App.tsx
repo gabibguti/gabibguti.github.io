efcf84f58a5c428f651079884a6c36b3836940cc
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-02-20 15:48:02 -0300

    feat: Add github icon on page top

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, { ReactElement } from 'react'
+import { IconGitHub } from './assets/IconGitHub'
 
 export function App(): ReactElement {
   return (
-    <div className="bg-gradient-to-r from-light-blue to-dark-cornflower-blue flex justify-center items-center px-24 w-screen h-screen">
-      <div className="flex flex-col text-center">
+    <div className="bg-gradient-to-r from-light-blue to-dark-cornflower-blue flex flex-col w-screen h-screen">
+      <div className="flex flex-row p-4 justify-end text-xiketic fill-current">
+        <IconGitHub />
+      </div>
+      <div className="flex flex-col text-center justify-center items-center h-full">
         <span className="text-5xl font-bold text-xiketic">
           Gabriela Gutierrez
         </span>
