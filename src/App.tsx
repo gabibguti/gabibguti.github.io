391494196333937a10230a70473eb3e847ef9fb7
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-02-20 15:56:37 -0300

    feat: Add linkedin icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React, { ReactElement } from 'react'
 import { IconGitHub } from './assets/IconGitHub'
+import { IconLinkedIn } from './assets/IconLinkedIn'
 
 export function App(): ReactElement {
   return (
     <div className="bg-gradient-to-r from-light-blue to-dark-cornflower-blue flex flex-col w-screen h-screen">
-      <div className="flex flex-row p-4 justify-end ">
-        <a
-          className="text-xiketic fill-current"
-          href="https://github.com/gabibguti"
-        >
-          <IconGitHub />
-        </a>
+      <div className="flex flex-row p-4 justify-end">
+        <div className="grid grid-flow-col grid-cols-2 gap-2">
+          <a
+            className="text-xiketic fill-current"
+            href="https://github.com/gabibguti"
+          >
+            <IconGitHub />
+          </a>
+          <IconLinkedIn />
+        </div>
       </div>
       <div className="flex flex-col text-center justify-center items-center h-full">
         <span className="text-5xl font-bold text-xiketic">
