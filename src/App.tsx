28b970c5e9f466b0a2bc5910f4a9708e01df7f42
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-02-21 10:47:41 -0300

    fix: Icons fill color

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,17 @@ export function App(): ReactElement {
   return (
     <div className="bg-gradient-to-r from-light-blue to-dark-cornflower-blue flex flex-col w-screen h-screen">
       <div className="flex flex-row px-6 py-4 justify-end">
-        <div className="grid grid-flow-col grid-cols-2 gap-3 text-xiketic fill-current">
-          <a href="https://github.com/gabibguti">
+        <div className="grid grid-flow-col grid-cols-2 gap-3">
+          <a
+            href="https://github.com/gabibguti"
+            className="text-xiketic fill-current"
+          >
             <IconGitHub />
           </a>
-          <a href="https://www.linkedin.com/in/gabriela-gutierrez-4213b0178/">
+          <a
+            href="https://www.linkedin.com/in/gabriela-gutierrez-4213b0178/"
+            className="text-xiketic fill-current"
+          >
             <IconLinkedIn />
           </a>
         </div>
