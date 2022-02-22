8e32be31cfd0b5a66152c36fff14545a4986f22f
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-02-21 22:15:40 -0300

    Updates

---
+++ precache-manifest.ccfedd551611aa1f13f2b8a24bb2d7f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17235e7bdc216b1bb393c5a6637f743d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fc36c58eaaefdc48495",
+    "url": "/static/css/main.64807cbe.chunk.css"
+  },
+  {
+    "revision": "0e028f488c62eb8f9b08",
+    "url": "/static/js/2.45eeb2eb.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.45eeb2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09312c15493d76d77aae",
+    "url": "/static/js/3.1d420e4c.chunk.js"
+  },
+  {
+    "revision": "7fc36c58eaaefdc48495",
+    "url": "/static/js/main.9e6addc3.chunk.js"
+  },
+  {
+    "revision": "bb9fe8bf36ae92337252",
+    "url": "/static/js/runtime-main.25b2f314.js"
+  }
+]);
