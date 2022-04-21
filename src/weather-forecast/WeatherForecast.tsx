c0e74dbb0a08711b00e27e2f36bd81a5f1bc761c
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-04-21 12:56:18 -0300

    style: Weather forecast title

--- src/weather-forecast/WeatherForecast.tsx
+++ src/weather-forecast/WeatherForecast.tsx
@@ -1,5 +1,13 @@
 import React, { ReactElement } from 'react'
 
 export function WeatherForecast(): ReactElement {
-  return <div className="flex flex-row">Weather Forecast</div>
+  return (
+    <div className="flex flex-col w-screen h-screen p-6">
+      <div className="flex flex-col items-center text-center h-full">
+        <span className="text-4xl font-extralight text-xiketic pb-4">
+          Weather Forecast
+        </span>
+      </div>
+    </div>
+  )
 }
