87bc6d52498ef6658175883f1ac0db2859629bf8
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-04-22 18:25:53 -0300

    style: Change city and temperature arrangement

--- src/weather-forecast/WeatherForecast.tsx
+++ src/weather-forecast/WeatherForecast.tsx
@@ -4,14 +4,16 @@ import { useWeather } from './useWeather'
 export function WeatherForecast(): ReactElement {
   const { data } = useWeather()
 
+  const city = 'Brasilia'
+
   return (
     <div className="flex flex-col w-screen h-screen p-9">
-      <div className="flex flex-col items-center text-center h-full">
-        <span className="text-4xl font-extralight text-xiketic pb-6">
-          Weather Forecast
+      <div className="flex flex-col items-center h-full">
+        <span className="flex flex-row font-light text-xl text-xiketic">
+          {city}
         </span>
-        <span className="text-3xl font-extralight text-xiketic">
-          {data?.temperature}&nbsp;{data?.temperatureUnit} Brasilia
+        <span className="flex flex-row font-thin text-xiketic text-8xl py-6">
+          {data?.temperature}&nbsp;{data?.temperatureUnit}
         </span>
       </div>
     </div>
