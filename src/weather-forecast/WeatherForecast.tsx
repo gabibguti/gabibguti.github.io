885b7ffe207165d7449a850df92a9a783af44819
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-04-22 23:52:50 -0300

    feat: Display next 3 hours forecast

--- src/weather-forecast/WeatherForecast.tsx
+++ src/weather-forecast/WeatherForecast.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import { FormattedDate } from 'react-intl'
+import { FormattedDate, FormattedTime } from 'react-intl'
 import IconLocation from '../assets/IconLocation'
 import IconWind from '../assets/IconWind'
 import { useWeather } from './useWeather'
@@ -21,7 +21,7 @@ export function WeatherForecast(): ReactElement {
         </span>
         <span className="flex flex-row font-light text-sm text-xiketic items-center pt-4">
           <FormattedDate
-            value={data?.time}
+            value={data?.currentWeather.time}
             year="numeric"
             month="long"
             day="2-digit"
@@ -31,15 +31,40 @@ export function WeatherForecast(): ReactElement {
           />
         </span>
         <span className="flex flex-row font-thin text-xiketic text-8xl py-6">
-          {data?.temperature}&nbsp;{data?.temperatureUnit}
+          {data?.currentWeather.temperature}&nbsp;{data?.temperatureUnit}
         </span>
         <span className="flex flex-row font-light text-lg text-xiketic items-center">
           <span className="pr-3">
             <IconWind />
           </span>
-          {data?.windSpeed}&nbsp;
+          {data?.currentWeather.windSpeed}&nbsp;
           <span className="tracking-wider">{windSpeedUnit}</span>
         </span>
+        <div className="flex flex-col text-xiketic items-center pt-14">
+          <span className="text-sm font-semibold uppercase pb-4">
+            Next Hours
+          </span>
+          <table className="table-auto font-light">
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.nextHoursForecast.map(({ time, temperature }, index) => (
+                <tr key={index}>
+                  <td className="text-right">{temperature}</td>
+                  <td>{data?.temperatureUnit}</td>
+                  <td className="text-right w-24">
+                    <FormattedTime value={time} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
--- src/weather-forecast/useWeather.ts
+++ src/weather-forecast/useWeather.ts
@@ -1,12 +1,22 @@
 import { useQuery } from 'react-query'
 import { fetcher } from '../utils'
 
+export interface HourlyWeather {
+  temperature: number
+  time: Date
+}
+
 interface CurrentWeather {
+  temperature: number
+  windSpeed: number
+  time: Date
+}
+
+interface Weather {
   data?: {
-    temperature: number
+    currentWeather: CurrentWeather
+    nextHoursForecast: Array<HourlyWeather>
     temperatureUnit: string
-    windSpeed: number
-    time: Date
   }
 }
 
@@ -25,7 +35,7 @@ interface OpenMeteoRes {
   }
 }
 
-export function useWeather(): CurrentWeather {
+export function useWeather(): Weather {
   const { data } = useQuery<OpenMeteoRes, Error>('todos', () =>
     fetcher<OpenMeteoRes>(
       'https://api.open-meteo.com/v1/forecast?latitude=-15.7801&longitude=-47.9292&hourly=temperature_2m&current_weather=true&timezone=America%2FSao_Paulo'
@@ -36,7 +46,9 @@ export function useWeather(): CurrentWeather {
     return {}
   }
 
-  const time = new Date(data?.current_weather.time)
+  const timeISO = data?.current_weather.time
+
+  const time = new Date(timeISO)
 
   const temperature = data?.current_weather.temperature
 
@@ -44,7 +56,23 @@ export function useWeather(): CurrentWeather {
 
   const windSpeed = data?.current_weather.windspeed
 
+  const currHourIndex = data?.hourly.time.findIndex((hour) => hour === timeISO)
+
+  const nextHours = data?.hourly.time.slice(
+    currHourIndex + 1,
+    currHourIndex + 4
+  )
+
+  const nextHoursForecast = nextHours.map((time, index) => ({
+    temperature: data?.hourly.temperature_2m[index],
+    time: new Date(time),
+  }))
+
   return {
-    data: { temperature, temperatureUnit, windSpeed, time },
+    data: {
+      currentWeather: { temperature, windSpeed, time },
+      nextHoursForecast,
+      temperatureUnit,
+    },
   }
 }
