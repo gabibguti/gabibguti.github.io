dd72571dc9cccf58d576b6c58bd0aa2079b2f498
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-12-30 13:04:53 -0300

    style: Adjust movies and tv shows spacing and alignment

--- src/movies/TVShowsTable.tsx
+++ src/movies/TVShowsTable.tsx
@@ -4,8 +4,8 @@ import {FormattedList} from "react-intl"
 
 function TVShowDetails(TVShow: TVShow): ReactElement {
   return (
-    <div className="grid grid-cols-10 gap-2 bg-green-50 p-2 rounded-md w-full text-xiketic">
-      <div className="col-span-3 row-span-5 bg-indigo-500 p-2 rounded-md">
+    <div className="grid grid-cols-11 gap-2 bg-green-50 p-2 rounded-md w-full text-xiketic">
+      <div className="col-span-4 row-span-5 bg-indigo-500 p-2 rounded-md">
         <span className="flex items-center justify-center h-full text-3xl font-bold">
           {TVShow.title}
         </span>
@@ -67,14 +67,14 @@ function TVShowDetails(TVShow: TVShow): ReactElement {
         </div>
       </div>
 
-      <div className="col-span-7 col-start-4 p-2 rounded-md">
+      <div className="col-span-7 col-start-5 p-2 rounded-md">
         <div className="flex flex-col">
           <span className="text-xs">Genres</span>
           <div className='flex flex-row gap-1 mt-2'>{TVShow.genres.map(genre =><div className='rounded-full bg-indigo-100 text-xs py-1 px-2'>{genre}</div>)}</div>
         </div>
       </div>
 
-      <div className="col-span-7 col-start-4 p-2 rounded-md">
+      <div className="col-span-7 col-start-5 p-2 rounded-md">
         <div className="flex flex-col">
           <span className="text-xs">Recommended by</span>
           <span className='text-sm'>{TVShow['recommended-by'] ? 
