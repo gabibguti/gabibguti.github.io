7edefd76704ccad174f6fded3da3900253b82e64
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-12-29 21:36:23 -0300

    feat: List all watched movies

--- src/movies/Movies.tsx
+++ src/movies/Movies.tsx
@@ -1,17 +1,24 @@
 import React, { ReactElement } from 'react'
+import { MoviesTable } from './MoviesTable'
 import { TotalizerBox } from './TotalizerBox'
+import movies from './movies.json'
+import { Movie } from './movie'
 
 export function Movies(): ReactElement {
   return (
-    <div className="bg-gradient-to-r from-aero-blue to-light-blue flex flex-col w-screen h-screen p-9">
+    <div className="bg-gradient-to-r from-aero-blue to-light-blue flex flex-col w-screen min-h-screen h-full p-9">
       <div className="flex flex-col items-center h-full">
-        <span className='text-4xl font-bold text-xiketic mb-10'>Movies Wrap-Up 2022</span>
+        <span className="text-4xl font-bold text-xiketic mb-10">
+          Movies Wrap-Up 2022
+        </span>
         <div className="h-32 grid grid-rows-1 grid-cols-3 gap-4 w-full">
           <TotalizerBox title="Total movies watched" total={100} />
           <TotalizerBox title="Total TV Shows watched" total={100} />
           <TotalizerBox title="Total hours watched" total={100} unit="hour" />
         </div>
-
+        <div className="pt-8 w-full">
+          <MoviesTable movies={movies.movies as Movie[]} />
+        </div>
       </div>
     </div>
   )
---
+++ src/movies/MoviesTable.tsx
@@ -0,0 +1,82 @@
+import React, { ReactElement } from 'react'
+import type { Movie } from './movie'
+
+// "title": string,
+// "sequence": number,
+// "time-min": number,
+// "year": number,
+// "genres": string[],
+// "grade-imdb": number,
+// "grade-gabriela": number,
+// "strong-points"?: string,
+// "weak-points"?: string,
+// "recommended-by"?: string[],
+// "rewatch": boolean
+
+function MovieDetails(movie: Movie): ReactElement {
+  return (
+    <div className="grid grid-cols-8 gap-2 bg-green-50 p-2 rounded-md w-full text-xiketic">
+      <div className="col-span-3 row-span-3 bg-indigo-500 p-2 rounded-md">
+        <span className="flex items-center justify-center h-full text-3xl font-bold">
+          {movie.title}
+        </span>
+      </div>
+      <div className="col-span-1 bg-indigo-300 p-2 rounded-md">
+        <div className="flex flex-col">
+          <span className="text-xs font-bold">IMDB Grade</span>
+          <span className="font-bold">{movie['grade-imdb']}</span>
+        </div>
+      </div>
+      <div className="col-span-1 bg-indigo-300 p-2 rounded-md">
+        <div className="flex flex-col">
+          <span className="text-xs font-bold">Grade</span>
+          <span className="font-bold">{movie['grade-gabriela']}</span>
+        </div>
+      </div>
+      <div className="col-span-1 bg-indigo-200 p-2 rounded-md">
+        <div className="flex flex-col">
+          <span className="text-xs">Time</span>
+          <span>{movie['time-min']} min</span>
+        </div>
+      </div>
+      <div className="col-span-1 bg-indigo-200 p-2 rounded-md">
+        <div className="flex flex-col">
+          <span className="text-xs">Year</span>
+          <span>{movie.year}</span>
+        </div>
+      </div>
+      <div className="col-span-1 bg-indigo-200 p-2 rounded-md">
+        <div className="flex flex-col">
+          <span className="text-xs">Rewatching?</span>
+          <span>{movie.rewatch ? 'Yes' : 'No'}</span>
+        </div>
+      </div>
+
+      <div className="col-span-5 bg-indigo-200 p-2 rounded-md">
+        <div className="flex flex-col">
+          <span className="text-xs">Strong points</span>
+          <span>{movie['strong-points'] || '-'}</span>
+        </div>
+      </div>
+
+      <div className="col-span-5 bg-indigo-200 p-2 rounded-md">
+        <div className="flex flex-col">
+          <span className="text-xs">Weak points</span>
+          <span>{movie['weak-points'] || '-'}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function MoviesTable({ movies }: { movies: Movie[] }): ReactElement {
+  return (
+    <>
+      {movies.map((movie) => (
+        <div className="py-2">
+          <MovieDetails {...movie} />
+        </div>
+      ))}
+    </>
+  )
+}
