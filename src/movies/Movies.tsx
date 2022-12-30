e5f7d446656efcd58a2d14e3cab180fedca76d16
Gabriela Gutierrez <gabibguti1@gmail.com>
2022-12-29 22:15:45 -0300

    feat: Calculate totalizers

--- src/movies/Movies.tsx
+++ src/movies/Movies.tsx
@@ -5,6 +5,16 @@ import movies from './movies.json'
 import { Movie } from './movie'
 
 export function Movies(): ReactElement {
+
+  const totalMoviesWatched = movies.movies.length
+  const totalTVShowsWatched = movies['tv-shows'].length
+
+  const totalMinWatched = movies.movies.reduce((totalTime, movie) => {
+    totalTime += movie['time-min']
+    return totalTime
+  }, 0)
+  const totalHoursWatched = Math.round(totalMinWatched / 60)
+
   return (
     <div className="bg-gradient-to-r from-aero-blue to-light-blue flex flex-col w-screen min-h-screen h-full p-9">
       <div className="flex flex-col items-center h-full">
@@ -12,9 +22,9 @@ export function Movies(): ReactElement {
           Movies Wrap-Up 2022
         </span>
         <div className="h-32 grid grid-rows-1 grid-cols-3 gap-4 w-full">
-          <TotalizerBox title="Total movies watched" total={100} />
-          <TotalizerBox title="Total TV Shows watched" total={100} />
-          <TotalizerBox title="Total hours watched" total={100} unit="hour" />
+          <TotalizerBox title="Total movies watched" total={totalMoviesWatched} />
+          <TotalizerBox title="Total TV Shows watched" total={totalTVShowsWatched} />
+          <TotalizerBox title="Total hours watched" total={totalHoursWatched} unit="hour" />
         </div>
         <div className="pt-8 w-full">
           <div className='rounded-md bg-gray-800 text-aero-blue mb-2 py-5 px-5'>
